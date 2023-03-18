384468582ce74de7e1931540d0d902055944f4ef
AhmedSHelmy <ahmed.s.helmy649@gmail.com>
2023-03-18 14:37:18 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main (void){
+/**
+ * main - entry point
+*
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
