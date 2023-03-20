5431cece43ee711973f8e4fd578bae6239ee722a
AhmedSHelmy <ahmed.s.helmy649@gmail.com>
2023-03-20 12:54:56 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+*
+* Return: returns nothing
+*/
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
