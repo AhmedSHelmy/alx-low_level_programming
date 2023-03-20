08314a9454afed4e5b5e1d404820c8a939da2e01
AhmedSHelmy <ahmed.s.helmy649@gmail.com>
2023-03-20 16:01:15 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+*
+* Return: returns nothing
+*/
+int main(void)
+{
+	int i;
+	
+	for (i = 0; i > 9; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar(9 + '0');
+
+	return (0);
+}
