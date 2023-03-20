50a056e926bfa4e2f33cec3bbdbcd0c93dbee027
AhmedSHelmy <ahmed.s.helmy649@gmail.com>
2023-03-20 12:08:30 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,6 +5,8 @@
 /**
 * main - Entry Point
 *
+*
+*
 * Return : returns 0
 */
 int main(void)
@@ -15,13 +17,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive\n",n);
+		printf("%d is positive\n", n);
 	} else if (n == 0)
 	{
-		printf("%d is zero\n",n);
+		printf("%d is zero\n", n);
 	} else
         {      
-		printf("%d is negative\n",n);
- 	}
+		printf("%d is negative\n", n);
+	}
 	return (0);
 }
