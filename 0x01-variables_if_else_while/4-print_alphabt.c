0b75e72d3069f712bb624805e1ca7ca68b42d737
AhmedSHelmy <ahmed.s.helmy649@gmail.com>
2023-03-20 14:52:53 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+*
+* Return: returns nothing
+*/
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c == 'e' || C == 'q')
+		{
+			c++;
+		} else
+		{
+			putchar(c);
+			c++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
