8ce0915d6f32b6caaa34936c4aaf6032af2c9e55
AhmedSHelmy <ahmed.s.helmy649@gmail.com>
2023-03-20 16:41:14 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,12 +12,15 @@ int main(void)
 	
 	for (i = 0; i < 10;  i++)
 	{
-		for (j = i+1; j < 10 ; j++)
+		for (j = i + 1; j < 10 ; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
+			if (i < 8)
+			{
 			putchar(',');
 			putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
