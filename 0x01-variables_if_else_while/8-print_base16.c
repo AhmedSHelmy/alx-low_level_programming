94e593c430c5ed4e3344e1852bc4036df98cc418
AhmedSHelmy <ahmed.s.helmy649@gmail.com>
2023-03-20 15:50:51 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+*
+* Return: returns nothing
+*/
+int main(void)
+{
+	char l = '0';
+	char u = 'a';
+
+	while (l < '10')
+	{
+		putchar(l);
+		l++;
+	}
+	while (u <= 'f')
+	{
+		putchar(u);
+		u++;
+	}
+	putchar('\n');
+	return (0);
+}
