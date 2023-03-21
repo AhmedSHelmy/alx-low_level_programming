661d1753b0bc25a14ede8c7015849df6beba4824
AhmedSHelmy <ahmed.s.helmy649@gmail.com>
2023-03-20 18:54:14 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,30 +9,20 @@ int main(void)
 {
 	int i;
 	int j;
-	int k;
-	int l;
-	int n = 1;
 
-	for (i = 0; i < 10;  i++)
+	for (i = 0; i <= 98;  i++)
 	{
-		for (j = 0 ; j < 10 ; j++)
-		{
-			for (k = 0 ; k < 10; k++)
+		for (j = i + 1 ; j <= 99 ; j++)
+		{	
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i < 98 && j < 99)
 			{
-				for (l = n; l < 10; l++)
-				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(' ');
-					putchar(k + '0');
-					putchar(l + '0');
-					if (i < 9 && j < 9)
-					{
-					putchar(',');
-					putchar(' ');
-					}
-					n = 0;
-				}
+			putchar(',');
+			putchar(' ');
 			}
 		}
 	}
