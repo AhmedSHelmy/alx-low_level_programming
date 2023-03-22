dcd99873c64e881c23efd2e5bba4a44bc65b5e36
AhmedSHelmy <ahmed.s.helmy649@gmail.com>
2023-03-22 07:11:12 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,13 @@
 */
 int main(void)
 {
-	putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
-}
-
-int _putchar()
-{
-	bool x;
-
-	while (x = true)
-	{
-		put
 }
