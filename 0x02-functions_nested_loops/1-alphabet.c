89e4666ebc50edfe8a4fed3ff9f6cdaff3ef4719
AhmedSHelmy <ahmed.s.helmy649@gmail.com>
2023-03-22 09:25:54 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+print_alphabet - prints the alphabet followed by a new line
+*
+Return: nothing
+*/
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	for(a; a >= 'z';a++)
+	{
+		if (a =='z')
+		{
+			_putchar(a)
+			_putchar('\n')
+		}
+		_putchar(a)
+	}
+}
