0e4817e37df9bb37fac3edb2d7d6a2d183111fc1
AhmedSHelmy <ahmed.s.helmy649@gmail.com>
2023-03-22 10:28:29 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_alphabet - prints the alphabet followed by a new line
+*/
+void print_alphabet_10x(void)
+{
+	int i =0;
+	while (i != 10 )
+	{
+		char a = 'a';
+
+		while (a <= 'z')
+		{
+		_putchar(a);
+		a++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
