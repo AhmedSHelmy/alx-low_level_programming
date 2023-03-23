b947da664dc209d06fc404a9ccd72676723ba031
AhmedSHelmy <ahmed.s.helmy649@gmail.com>
2023-03-23 05:27:07 -0700

    Taskk 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,6 @@
 #include "main.h"
+
+void print_number(int x);
 /**
 * times_table - prints the 9 times table, starting with 0.
 *
@@ -10,48 +12,47 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
-		{
-			int x = i * j;
+		int x = i * j;
 
+		for (j = 0; j < 9; j++)
+		{
 			if (j == 0)
 			{
 				_putchar(x + '0');
 				_putchar(',');
 			}
-			else if (j == 9)
+			else if ( j == 9)
 			{
-				if (x >= 10)
-				{
-					_putchar(' ');
-					_putchar(x / 10 + '0');
-					_putchar(x % 10 + '0');
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(x + '0');
-				}
+				print_number(x);
 			}
-			else
+			else 
 			{
-				if (x >= 10)
-				{
-					_putchar(' ');	
-					_putchar(x / 10 + '0');
-					_putchar(x % 10 + '0');
-					_putchar(',');
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(x + '0');
-					_putchar(',');	
-				}
+				print_number(x);
+				_putchar(',');
 			}
 		}
 		_putchar('\n');
 	}
 }
+
+/**
+* print_number - print
+* @x: number to print
+*
+* Return: nothing
+*/
+void print_number(int x)
+{
+	if (x >= 10)
+	{
+		_putchar(' ');
+		_putchar(x + '0');
+	}
+	else
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(x + '0');
+	}
+}
+
