3be22dbf3ca4a662fc9d014f2a31c1922c721a4f
AhmedSHelmy <ahmed.s.helmy649@gmail.com>
2023-03-22 19:00:07 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,12 +7,13 @@
 void times_table(void)
 {
 	int i, j;
+	int x = i * j;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			_putchar(_abs(i * j) + '0');
+			_putchar(_abs(x) + '0');
 			_putchar(',');
 			_putchar(' ');
 			_putchar(' ');
